051b6895aedf91aafb2b48cd6bb1207299facd73
LLBotelho <103432015+LLBotelho@users.noreply.github.com>
2022-05-12 19:42:20 -0300

    Update pset2.sql

--- pset2.sql
+++ pset2.sql
@@ -1,316 +1,135 @@
---QUESTÃO 01:
+-- Questão 1:
 
-SELECT 
-	f.numero_departamento AS "número do departamento",
-	d.nome_departamento AS "nome do departamento",
-	CAST(AVG(f.salario) AS MONEY) AS "média total do salário"
-FROM
-	funcionario AS f
-	INNER JOIN
-	departamento AS d
-	ON
-		(f.numero_departamento = d.numero_departamento)
-GROUP BY
-	d.numero_departamento, f.numero_departamento
-ORDER BY
-	d.numero_departamento ASC;
+SELECT d.numero_departamento, AVG(salario) AS media_salarial
+FROM funcionarios AS f, departamento AS d
+WHERE d.numero_departamento = f.numero_departamento
+GROUP BY d.numero_departamento ;
 
-----------------------------------------||----------------------------------------
+-- Questão 2:
 
---QUESTÃO 02:
+SELECT f.sexo, CAST(AVG(salario) AS DECIMAL (10, 2)) AS média 
+FROM funcionarios AS f
+WHERE (sexo = 'M')
+GROUP BY f.sexo
+UNION 
+SELECT f.sexo, CAST(AVG(salario) AS DECIMAL (10, 2)) 
+FROM funcionarios AS f
+WHERE (sexo = 'F')
+GROUP BY f.sexo;
 
-SELECT
-	CASE f.sexo
-	 	WHEN 'M' THEN 'Masculino'
-		WHEN 'F' THEN 'Feminino'
-	END AS "Sexo",
-	CAST(AVG(f.salario) AS MONEY) AS "Média Salarial"
-FROM
-	funcionario AS f
-GROUP BY
-	f.sexo
-ORDER BY
-	f.sexo ASC;
+-- Questão 3:
 
-----------------------------------------||----------------------------------------
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, f.data_nascimento, CAST(f.salario AS DECIMAL(10,2)), d.nome_departamento, AGE(f.data_nascimento) AS idade
+FROM funcionarios AS f 
+INNER JOIN
+departamento AS d ON (f.numero_departamento = d.numero_departamento);
 
--- QUESTÃO 03:
+-- Questão 4:
 
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	TO_CHAR(f.data_nascimento, 'dd/mm/yyyy') AS "Data de Nascimento",
-	DATE_PART('year', AGE(CURRENT_DATE, f.data_nascimento)) AS "Idade",
-	CAST(f.salario AS MONEY) AS "Salário"
-FROM
-	departamento AS d
-	INNER JOIN
-	funcionario AS f
-ON
-	(d.numero_departamento = f.numero_departamento)
-ORDER BY
-	d.nome_departamento ASC;
-
-----------------------------------------||----------------------------------------
-
--- QUESTÃO 04:
-
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	DATE_PART('year', AGE(CURRENT_DATE, f.data_nascimento)) AS "Idade",
-	CAST(f.salario AS MONEY) AS "Salário Atual",
-	CAST(f.salario * 1.2 AS MONEY) AS "Salário com Reajuste"
-FROM
-	funcionario AS f
-WHERE
-	f.salario < 35000
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, CAST(f.salario AS DECIMAL (10,2)) AS salario, CAST (f.salario*1.2 AS DECIMAL (10,2)) AS salario_reajustado, d.nome_departamento
+FROM funcionarios AS f, departamento AS d
+WHERE salario < 35000 AND f.numero_departamento = d.numero_departamento
 UNION
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	DATE_PART('year', AGE(CURRENT_DATE, f.data_nascimento)) AS "Idade",
-	CAST(f.salario AS MONEY) AS "Salário Atual",
-	CAST(f.salario * 1.15 AS MONEY) AS "Salário com Reajuste"
-FROM
-	funcionario AS f
-WHERE
-	f.salario >= 35000
-ORDER BY
-	"Nome do Funcionário" ASC;
+SELECT (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, CAST(f.salario AS DECIMAL (10,2)) AS salario, CAST (f.salario*1.15 AS DECIMAL (10,2)) AS salario_reajustado, d.nome_departamento
+FROM funcionarios AS f, departamento AS d
+WHERE salario >= 35000 AND f.numero_departamento = d.numero_departamento;
 
-----------------------------------------||----------------------------------------
+-- Questão 5:
 
--- QUESTÃO 05:
+SELECT d.nome_departamento, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_completo, f.salario
+FROM departamento AS d, funcionarios AS f
+WHERE d.numero_departamento = f.numero_departamento
+ORDER BY d.nome_departamento ASC, f.salario DESC;
 
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	CONCAT(f2.primeiro_nome, ' ', f2.nome_meio, '. ', f2.ultimo_nome) AS "Nome do Gerente",
-	CONCAT(f1.primeiro_nome, ' ', f1.nome_meio, '. ', f1.ultimo_nome) AS "Nome do Funcionario"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	funcionario AS f1
-	INNER JOIN
-	funcionario AS f2
-	ON
-		d.cpf_gerente = f2.cpf
-ORDER BY
-	d.nome_departamento ASC,
-	f1.salario DESC;
+-- Questão 6:
 
-----------------------------------------||----------------------------------------
+SELECT f.numero_departamento, (f.primeiro_nome ||' '|| f.nome_meio ||' '|| f.ultimo_nome) AS nome_funcionario, d.nome_dependente, AGE(d.data_nascimento) AS idade_dependente, (case when(d.sexo = 'M')then 'Masculino' when(d.sexo = 'F')then 'Feminino'end) as sexo
+FROM funcionarios AS f, dependente AS d
+WHERE f.cpf = d.cpf_funcionario;
 
--- QUESTÃO 06:
+-- Questão 7:
 
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	d.nome_departamento AS "Nome do Departamento",
-	dep.nome_dependente AS "Nome do Dependente",
-	DATE_PART('year', AGE(CURRENT_DATE, dep.data_nascimento)) AS "Idade do Dependente",
-	CASE dep.sexo
-		WHEN 'M' THEN 'Masculino'
-		WHEN 'F' THEN 'Feminino'
-	END AS "Sexo do Dependente"
-FROM
-	funcionario AS F
-	INNER JOIN
-	departamento AS d
-	ON
-		f.numero_departamento = d.numero_departamento
-	INNER JOIN
-	dependente AS dep
-	ON
-		f.cpf = dep.cpf_funcionario
-ORDER BY
-	"Idade do Dependente" ASC;
+SELECT (f.primeiro_nome||' '|| f.nome_meio||' '|| f.ultimo_nome) AS nome_funcionario, f.numero_departamento, f.salario
+FROM funcionarios AS f
+WHERE f.cpf NOT IN (SELECT d.cpf_funcionario FROM dependente AS d);
 
-----------------------------------------||----------------------------------------
+-- Questão 8:
 
--- QUESTÃO 07:
+SELECT d.nome_departamento, p.nome_projeto, (f.primeiro_nome||' '|| f.nome_meio||' '|| f.ultimo_nome) AS nome_funcionario, t.horas
+FROM funcionarios AS f
+INNER JOIN trabalha_em AS t
+ON f.cpf = t.cpf_funcionario
+INNER JOIN projeto AS p
+ON t.numero_projeto = p.numero_projeto
+INNER JOIN departamento AS d
+ON p.numero_departamento = d.numero_departamento
+ORDER BY d.nome_departamento, p.nome_projeto, f.salario DESC;
 
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	d.nome_departamento AS "Nome do Departamento",
-	CAST(f.salario AS MONEY) AS "Salário"
-FROM
-	funcionario AS f
-	INNER JOIN
-	departamento AS d
-	ON
-		f.numero_departamento = d.numero_departamento
-WHERE NOT EXISTS
-	(
-		SELECT
-			*
-		FROM
-			dependente AS dep
-		WHERE
-			f.cpf = dep.cpf_funcionario
-	)
-ORDER BY
-	"Nome do Funcionário" ASC;
+-- Questão 9:
+SELECT d.nome_departamento, p.nome_projeto, SUM(T.horas) AS total_horas
+FROM funcionarios AS f
+INNER JOIN trabalha_em AS t
+ON f.cpf = t.cpf_funcionario
+INNER JOIN projeto AS p
+ON t.numero_projeto = p.numero_projeto
+INNER JOIN departamento AS d
+ON p.numero_departamento = d.numero_departamento
+GROUP BY d.nome_departamento, p.nome_projeto;
 
-----------------------------------------||----------------------------------------
+-- Questão 10:
 
--- QUESTÃO 08:
+SELECT d.numero_departamento, AVG(salario) AS media_salarial
+FROM funcionarios AS f, departamento AS d
+WHERE d.numero_departamento = f.numero_departamento
+GROUP BY d.numero_departamento ;
 
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	p.nome_projeto AS "Nome do Projeto",
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	te.horas AS "Horas Trabalhadas"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	projeto AS p
-	NATURAL JOIN
-	trabalha_em AS te
-	INNER JOIN
-	funcionario AS f
-	ON
-		f.cpf = te.cpf_funcionario
-ORDER BY
-	d.nome_departamento ASC,
-	p.nome_projeto ASC,
-	"Nome do Funcionário" ASC;
+-- Questão 11:
 
-----------------------------------------||----------------------------------------
+SELECT (f.primeiro_nome||' '||f.nome_meio||' '|| f.ultimo_nome) AS nome_funcionario, p.nome_projeto, CAST (t.horas * 50 AS DECIMAL(10,2)) AS valor_total
+FROM funcionarios AS f
+INNER JOIN trabalha_em AS t
+ON f.cpf = t.cpf_funcionario
+INNER JOIN projeto AS p
+ON t.numero_projeto = p.numero_projeto
+ORDER BY f.salario DESC;
 
--- QUESTÃO 09:
+-- Questão 12:
 
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	p.nome_projeto AS "Nome do Projeto",
-	SUM(te.horas) AS "Total de Horas Trabalhadas"
-FROM
-	trabalha_em AS te
-	NATURAL JOIN
-	projeto AS p
-	NATURAL JOIN
-	departamento AS d
-GROUP BY
-	d.nome_departamento,
-	p.nome_projeto
-ORDER BY
-	d.nome_departamento ASC,
-	p.nome_projeto ASC;
+SELECT d.nome_departamento, p.nome_projeto, (f.primeiro_nome||' '|| f.nome_meio||' '|| f.ultimo_nome) AS nome_funcionario
+FROM funcionarios AS f
+INNER JOIN departamento AS d
+ON f.numero_departamento = d.numero_departamento
+INNER JOIN trabalha_em AS t
+ON f.cpf = t.cpf_funcionario
+INNER JOIN projeto AS p
+ON t.numero_projeto = p.numero_projeto
+WHERE t.horas IS NULL;
 
-----------------------------------------||----------------------------------------
+-- Questão 13:
 
--- QUESTÃO 10:
-
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	CAST(AVG(f.salario) AS MONEY) AS "Média Salarial"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	funcionario AS f
-GROUP BY
-	d.nome_departamento
-ORDER BY
-	d.nome_departamento ASC;
-
-----------------------------------------||----------------------------------------
-
--- QUESTÃO 11:
-
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	p.nome_projeto AS "Nome do Projeto",
-	CAST(50 * te.horas AS MONEY) AS "Valor"
-FROM
-	trabalha_em AS te
-	NATURAL JOIN
-	projeto AS p
-	INNER JOIN
-	funcionario AS f
-	ON
-		te.cpf_funcionario = f.cpf
-ORDER BY
-	"Nome do Funcionário" ASC,
-	"Nome do Projeto" ASC;
-
-----------------------------------------||----------------------------------------
-
--- QUESTÃO 12:
-
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	p.nome_projeto AS "Nome do Projeto",
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	projeto AS p
-	NATURAL JOIN
-	funcionario AS f
-	INNER JOIN
-	trabalha_em AS te
-	ON
-		te.cpf_funcionario = f.cpf
-WHERE
-	te.horas IS NULL;
-
-----------------------------------------||----------------------------------------
-
--- QUESTÃO 13:
-
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome",
-	CASE f.sexo
-		WHEN 'F' THEN 'Feminino'
-		WHEN 'M' THEN 'Masculino'
-	END AS "Sexo",
-	DATE_PART('year', AGE(CURRENT_DATE, f.data_nascimento)) AS "Idade"
-FROM
-	funcionario AS f
+SELECT (f.primeiro_nome||' '||f.nome_meio||' '||f.ultimo_nome) AS nome, (case when (f.sexo='m') then 'Masculino' when (f.sexo='f') then 'Feminino'end) as sexo, DATE_PART('year', AGE(f.data_nascimento)) AS idade
+FROM funcionarios AS f
 UNION
-SELECT
-	dep.nome_dependente AS "Nome",
-	CASE dep.sexo
-		WHEN 'F' THEN 'Feminino'
-		WHEN 'M' THEN 'Masculino'
-	END AS "Sexo",
-	DATE_PART('year', AGE(CURRENT_DATE, dep.data_nascimento)) AS "Idade"
-FROM
-	dependente AS dep
-ORDER BY
-	"Idade" DESC;
+SELECT (d.nome_dependente) AS nome, (case when (d.sexo='M') then 'Masculino' when (d.sexo='F') then 'Feminino'end) as sexo, DATE_PART('year', AGE(d.data_nascimento)) AS idade
+FROM dependente AS d
+ORDER BY idade DESC; 
 
-----------------------------------------||----------------------------------------
+-- Questão 14:
 
--- QUESTÃO 14:
+SELECT d.nome_departamento, COUNT(f.cpf) AS quantidade_funcionarios
+FROM funcionarios AS f
+INNER JOIN departamento AS d
+ON f.numero_departamento = d.numero_departamento
+GROUP BY d.nome_departamento;
 
-SELECT
-	d.nome_departamento AS "Nome do Departamento",
-	COUNT(f.cpf) AS "Número de Funcionários"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	funcionario AS f
-GROUP BY
-	d.numero_departamento
-ORDER BY
-	"Nome do Departamento" ASC;
+-- Questão 15:
 
-----------------------------------------||----------------------------------------
-
--- QUESTÃO 15:
-
-SELECT
-	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
-	d.nome_departamento AS "Nome do Departamento",
-	p.nome_projeto AS "Nome do Projeto"
-FROM
-	departamento AS d
-	NATURAL JOIN
-	projeto AS p
-	NATURAL JOIN
-	trabalha_em AS te
-	RIGHT OUTER JOIN
-	funcionario AS f
-	ON
-		te.cpf_funcionario = f.cpf
-ORDER BY
-	"Nome do Funcionário" ASC,
-	"Nome do Departamento" ASC;
+SELECT (F.primeiro_nome||''|| F.nome_meio||''||F.ultimo_nome) AS nome_funcionario, d.nome_departamento, p.nome_projeto
+FROM funcionarios AS f
+INNER JOIN trabalha_em AS t
+ON f.cpf = t.cpf_funcionario
+INNER JOIN projeto AS p
+ON t.numero_projeto = p.numero_projeto
+INNER JOIN departamento AS d
+ON f.numero_departamento = d.numero_departamento
+ORDER BY nome_funcionario;
